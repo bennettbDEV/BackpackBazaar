10c72af6074342e608f0d1eb822aa2387c501bb3
bennettbDEV <bennettbeltran1@gmail.com>
2025-01-14 22:50:17 -0600

    Adding with_user action to list all messages between current user and other specified user

--- backend/user_messages/views.py
+++ backend/user_messages/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
+from django.db import models
 from rest_framework import status, viewsets
+from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -34,3 +37,30 @@ class MessageViewSet(viewsets.ModelViewSet):
         if message.sender != request.user:
             raise PermissionDenied("You are not allowed to delete this message.")
         return super().destroy(request, *args, **kwargs)
+
+    # List messages between the current user and another user specified by user_id
+    # Full url example: /messages/with_user/?user_id=1
+    @action(detail=False, methods=["get"], permission_classes=[IsAuthenticated])
+    def with_user(self, request):
+        user_id = request.query_params.get("user_id")
+        if not user_id:
+            return Response(
+                {"error": "user_id is required."}, status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            other_user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return Response(
+                {"error": "User not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        # models.Q used for more eloborate query
+        # to retrieve all messages between specified users
+        messages = Message.objects.filter(
+            (models.Q(sender=self.request.user) & models.Q(receiver=other_user))
+            | (models.Q(sender=other_user) & models.Q(receiver=self.request.user))
+        ).order_by("created_at")
+
+        serializer = self.get_serializer(messages, many=True)
+        return Response(serializer.data)
